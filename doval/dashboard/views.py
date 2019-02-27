cf64b013a128da009a7043907279b28eb32874c7
JulianDekker <Julian.dekker@live.nl>
2019-02-27 16:47:41 +0100

    Some annotating

--- doval/dashboard/views.py
+++ doval/dashboard/views.py
@@ -17,21 +17,11 @@ from bokeh.models.widgets import DataTable, DateFormatter, TableColumn
 
 params = {}
 datalist = []
-# Renders the dashboard of the application
-@ensure_csrf_cookie
-def homepage(request, file=None):
-    print(request, request.method)
-    if request.method == 'POST':
-        file = upload(request)
-        if file is not None:
-            params['filename'] = file.name
-            pandasInAction(file.name)
-            print("After this it should render ... ", request)
-            return render(request, 'pages/dashboard.html', params)
-    return render(request, 'pages/dashboard.html')
-
 
 class DragAndDropUploadView(View):
+    '''
+    Renders the dashboard and handles the drag and drop actions.
+    '''
     def get(self, request):
         datalist.clear()
         form = FileForm()
@@ -52,26 +42,56 @@ class DragAndDropUploadView(View):
         return JsonResponse(data)
 
 
-# Uploads the file in the request
-def upload(request):
-    print(request.FILES)
-    file = request.FILES[list(request.FILES)[0]]
-    fs = FileSystemStorage()
-    fs.save(file.name, file)
-    return file
-
-
-# Extracts basic information from a file
 def pandasInAction(filename):
+    '''
+    Pandas handle not implemented
+    :param filename:
+    :return:
+    '''
     df = pd.read_csv(settings.MEDIA_ROOT + '/' + filename)
     #params['shortfile'] = df.head(5)+df.tail(5)
     params['datadescription'] = df.describe()
     print(df.describe(include='all'))
 
+
 def clear_database():
+    '''
+    Deletes all files that are not stored in /doval/files/files
+    '''
     for dele in file.objects.all():
         if not os.path.isfile(settings.MEDIA_ROOT[0:-6] + dele.file.url):
             dele.file.delete()
             dele.delete()
     print('cleared db from non-existing files')
-    return
+
+
+@ensure_csrf_cookie
+def homepage(request, file=None):
+    '''
+    Deprecated dashboard render
+    :param request:
+    :param file:
+    :return:
+    '''
+    print(request, request.method)
+    if request.method == 'POST':
+        file = upload(request)
+        if file is not None:
+            params['filename'] = file.name
+            pandasInAction(file.name)
+            print("After this it should render ... ", request)
+            return render(request, 'pages/dashboard.html', params)
+    return render(request, 'pages/dashboard.html')
+
+
+def upload(request):
+    '''
+    Deprecated upload handler
+    :param request:
+    :return:
+    '''
+    print(request.FILES)
+    file = request.FILES[list(request.FILES)[0]]
+    fs = FileSystemStorage()
+    fs.save(file.name, file)
+    return file
